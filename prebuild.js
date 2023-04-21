dac27cd5131ee4eefb1d0c6a2c71dda41505df0f
Manav Sethi <manavsethi0505@gmail.com>
2023-04-22 04:13:03 +0530

    added prebuild script

--- prebuild.js
+++ prebuild.js
@@ -1,6 +1,31 @@
-[`REACT_APP_MAPBOX_ACCESS_TOKEN`].forEach((envVariable) => {
-  if (!process.env[envVariable]) {
-    console.error(`${envVariable} is not present in environment variables`);
-    process.exit(1);
+const fs = require('fs');
+
+// Read the .env file
+const envFile = fs.readFileSync('.env', 'utf-8');
+
+// Parse the contents of the .env file
+const envVariables = envFile.split('\n').reduce((acc, line) => {
+  const [key, value] = line.split('=');
+  if (key) {
+    acc[key] = value;
   }
+  return acc;
+}, {});
+
+// Set the environment variables
+Object.keys(envVariables).forEach((key) => {
+  process.env[key] = envVariables[key];
 });
+
+// check if the environment variables required are there
+const requiredVars = ['REACT_APP_MAPBOX_ACCESS_TOKEN'];
+
+const missingVars = requiredVars.filter((v) => !process.env[v]);
+
+if (missingVars.length) {
+  console.error(`Missing environment variable(s): ${missingVars.join(', ')}`);
+  process.exit(1);
+} else {
+  console.log('All environment variables are present.');
+  process.exit(0);
+}
