95616032ecdaf023c444f0520c2dd576119e03dd
Manav Sethi <manavsethi0505@gmail.com>
2023-04-22 13:21:33 +0530

    added check for dev env

--- prebuild.js
+++ prebuild.js
@@ -1,25 +1,28 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// // Read the .env file
-// const envFile = fs.readFileSync('.env', 'utf-8');
+// inject environment variables from .env file into process.env
+if (process.env.NODE_ENV === 'development') {
+  // Read the .env file
+  const envFile = fs.readFileSync('.env', 'utf-8');
 
-// // Parse the contents of the .env file
-// const envVariables = envFile.split('\n').reduce((acc, line) => {
-//   const [key, value] = line.split('=');
-//   if (key) {
-//     acc[key] = value;
-//   }
-//   return acc;
-// }, {});
+  // Parse the contents of the .env file
+  const envVariables = envFile.split('\n').reduce((acc, line) => {
+    const [key, value] = line.split('=');
+    if (key) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
 
-// // Set the environment variables
-// Object.keys(envVariables).forEach((key) => {
-//   process.env[key] = envVariables[key];
-// });
+  // Set the environment variables
+  Object.keys(envVariables).forEach((key) => {
+    process.env[key] = envVariables[key];
+  });
+}
 
-process.env.CI = false; // To prevent treating warnings as errors in CI mode
+// in production, the environment variables are injected via the CI config file (.github/worflows/deploy.yaml)
 
-// check if the environment variables required are there
+// check if the environment variables required are there in the environment
 const requiredVars = ['REACT_APP_MAPBOX_ACCESS_TOKEN'];
 
 const missingVars = requiredVars.filter((v) => !process.env[v]);
